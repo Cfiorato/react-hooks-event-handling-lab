56c65e0f92e9fa0a0aca1e490816c64557061f38
Cameron Fiorato <cameronclayfiorato@gmail.com>
2021-12-15 10:51:55 -0600

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+
+function Keypad(){
+  function inputHandler(){
+    console.log('Entering password...')
+  }
+  return <input type='password' onChange={inputHandler}/>
+}
+
+export default Keypad;
